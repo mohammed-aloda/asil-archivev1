06404ac6eb580e54776a54f62cdba764be265d11
mohammed-al-oda <aloda007@gmail.com>
2026-01-31 23:46:10 -0500

    Fix Stripe API version

--- api/create-checkout-session.ts
+++ api/create-checkout-session.ts
@@ -1,9 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-10-16',
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function handler(
   req: VercelRequest,
