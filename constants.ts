ffae1c8170c6072cbfbd2b11415738af12a6157a
mohammed-al-oda <aloda007@gmail.com>
2026-05-21 18:41:44 -0400

    Update products list in constants.ts with IRAQ Origins

--- constants.ts
+++ constants.ts
@@ -1,15 +1,17 @@
+import { Product } from './types';
+
 export const PRODUCTS: Product[] = [
   {
     "name": "IRAQ Origins (Pre-Order)",
     "price": 79.99,
     "category": "Cultural",
     "origin": "IRAQ",
-    "description": "Woven with the hues of rose-red stone, this silk tunic embodies the endurance of the Nabataeans.",
+    "description": "Infused with the proud heritage, culture and landmarks of Iraq.",
     "image": "/images/photo for website/Iraq V1 Front Jersey Photoshop.png",
     "images": [
       "/images/photo%20for%20website/Iraq%20V2%20back%20Jersey%20Photoshop.png"
     ],
-    "materials": "100% fr",
+    "materials": "Material: 100% Performance Polyester  Fabric Style: Engineered Micro-Mesh Knit  Print Type: Dye-Sublimation (Base) & Heat-Transfer (Flag)  Logos: Premium Raised Embroidery  Fit: Athletic / Regular",
     "id": "1"
   }
 ];
