e45e690206e73c27a52d194beacb42b5ceae5ba8
mohammed-al-oda <aloda007@gmail.com>
2026-05-25 01:03:52 -0400

    Add WebsiteIMG gallery images to IRAQ Origins product

--- constants.ts
+++ constants.ts
@@ -1,17 +1,25 @@
-import { Product } from './types';
-
 export const PRODUCTS: Product[] = [
   {
-    "name": "IRAQ Origins (Pre-Order)",
+    "name": "IRAQ Origins ",
     "price": 79.99,
     "category": "Cultural",
     "origin": "IRAQ",
     "description": "Infused with the proud heritage, culture and landmarks of Iraq.",
     "image": "/images/photo for website/Iraq V1 Front Jersey Photoshop.png",
     "images": [
-      "/images/photo%20for%20website/Iraq%20V2%20back%20Jersey%20Photoshop.png"
+      "/images/photo%20for%20website/Iraq%20V2%20back%20Jersey%20Photoshop.png",
+      "/images/photo%20for%20website/WebsiteIMG1.jpg",
+      "/images/photo%20for%20website/WebsiteIMG2.jpg",
+      "/images/photo%20for%20website/WebsiteIMG3.jpg",
+      "/images/photo%20for%20website/WebsiteIMG4.jpg",
+      "/images/photo%20for%20website/WebsiteIMG5.jpg",
+      "/images/photo%20for%20website/WebsiteIMG6.jpg",
+      "/images/photo%20for%20website/WebsiteIMG7.jpg",
+      "/images/photo%20for%20website/WebsiteIMG8.jpg"
     ],
     "materials": "Material: 100% Performance Polyester  Fabric Style: Engineered Micro-Mesh Knit  Print Type: Dye-Sublimation (Base) & Heat-Transfer (Flag)  Logos: Premium Raised Embroidery  Fit: Athletic / Regular",
+    "stripeProductId": "prod_Ua0eKlAdcfh1Q3",
+    "stripePriceId": "price_1TaqdcRqCqht2RR5dUHb2aZJ",
     "id": "1"
   }
 ];
